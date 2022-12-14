cba070323a1570b53311022935a9f8bf66d5839e
Luke Taylor <luke@deentaylor.com>
2022-12-14 16:02:02 -0500

    Fix shader scaling

--- src/canvas.ts
+++ src/canvas.ts
@@ -24,55 +24,49 @@ export function init(canvas: HTMLCanvasElement) {
   // Set clear color to a nice shade of green
   gl.clearColor(0.2, 0.8, 0.4, 1.0);
 
-  // Initialize VBO and VAO
-  const vertices = [
-    -1.0, -1.0, 0.0, 1.0,
-    1.0, -1.0, 0.0, 1.0,
-    1.0,  1.0, 0.0, 1.0,
-    -1.0, -1.0, 0.0, 1.0,
-    1.0,  1.0, 0.0, 1.0,
-    -1.0,  1.0, 0.0, 1.0,
-  ];
-  const vbo = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-
-  const vao = gl.createVertexArray();
-  gl.bindVertexArray(vao);
-  gl.enableVertexAttribArray(0);
-  gl.vertexAttribPointer(0, 4, gl.FLOAT, false, 0, 0);
-
-  // Generate 3D Worley noise texture
+  // Pre-generate 3D Worley noise texture
   const worleySize = 128;
   const worleyNumPoints = 4;
   const worleyRepeatTex = 1;
   const worleyTex = makeWorleyTexture(gl, worleyNumPoints, worleySize);
 
   // Initialize shader
-  const shader = makeProgram(gl, vertexShaderSrc, fragmentShaderSrc);
-  if (shader == null) return;
-  const locTex = gl.getUniformLocation(shader, 'tex');
-  const locResolution = gl.getUniformLocation(shader, 'resolution');
-  const locZ = gl.getUniformLocation(shader, 'z');
-  const locTile = gl.getUniformLocation(shader, 'tile');
+  const program = makeProgram(gl, vertexShaderSrc, fragmentShaderSrc);
+  if (program == null) return;
+  const locTex = gl.getUniformLocation(program, 'tex');
+  const locResolution = gl.getUniformLocation(program, 'resolution');
+  const locZ = gl.getUniformLocation(program, 'z');
+  const locTile = gl.getUniformLocation(program, 'tile');
+
+  // Set up geometry
+  const positionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([-1, 3, -1, -1, 3, -1]), gl.STATIC_DRAW);
+  // Bind vertex data to a_position attribute
+  const positionAttributeLocation = gl.getAttribLocation(program, 'a_position');
+  const vao = gl.createVertexArray();
+  gl.bindVertexArray(vao);
+  gl.enableVertexAttribArray(positionAttributeLocation);
+  gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
 
   // Make sure OpenGL is in correct state
-  gl.bindFramebuffer(gl.FRAMEBUFFER, null);
-  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-  gl.bindTexture(gl.TEXTURE_3D, worleyTex);
 
   // Set up render loop
   let frame: number;
   const draw = () => {
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+    gl.bindTexture(gl.TEXTURE_3D, worleyTex);
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    gl.clearColor(0, 0, 0, 0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
+    gl.useProgram(program);
     gl.bindVertexArray(vao);
-    gl.useProgram(shader);
     gl.uniform1i(locTex, 0);
     gl.uniform2f(locResolution, gl.canvas.width, gl.canvas.height);
     gl.uniform1f(locZ, frame / worleySize);
     gl.uniform1f(locTile, worleyRepeatTex);
-    gl.drawArrays(gl.TRIANGLES, 0, 6);
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
 
     // Schedule next frame
     frame = requestAnimationFrame(draw);
--- src/vert_passthrough.glsl
+++ src/vert_passthrough.glsl
@@ -1,7 +1,7 @@
 #version 300 es
 
-in vec4 position;
+in vec2 a_position;
 
 void main() {
-    gl_Position = position;
+    gl_Position = vec4(a_position, 0.0, 1.0);
 }
