3b1f001c2805fe2a0316d8d8546730bd20c2f8de
Luke Taylor <luke@deentaylor.com>
2022-12-17 16:49:10 -0500

    Add playground to viteo config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import glsl from 'vite-plugin-glsl';
 import { defineConfig } from 'vite';
 
+// make resolver
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
   plugins: [glsl()],
-  assetsInclude: ['**/*.fbx'],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        playground: resolve(__dirname, 'playground.html'),
+      },
+    },
+  },
 });
