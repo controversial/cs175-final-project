93e0b9213aa94f28e003ddc9515ac07de5a26ac9
Luke Taylor <luke@deentaylor.com>
2022-12-19 10:07:05 -0500

    Scroll sun position

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import './styles/index.scss';
 
-import Renderer, { SceneContext } from './renderer';
+import Renderer from './renderer';
 import { canvas, gl } from './context';
 
 import { loadBirdbath, renderBirdbath } from './scenes/birdbath';
@@ -8,14 +8,13 @@ import { loadWater, renderWater, updateWaves, waterHandleClick } from './scenes/
 import { renderCloudsWithContext } from './scenes/cloud';
 import { setupWorleyTexture } from './scenes/worley';
 import { loadGround, renderGround } from './scenes/ground';
-import { updateSunParameters } from './scenes/sky';
 
 const renderer = new Renderer(canvas, gl);
 
 setupWorleyTexture(64);
 
-renderer.addRenderStep((ctx: SceneContext) => updateSunParameters(ctx));
 renderer.addRenderStep(renderCloudsWithContext);
+
 Promise.all([loadGround(), loadBirdbath(), loadWater()]).then(() => {
   renderer.addRenderStep(renderGround);
   renderer.addRenderStep(renderBirdbath);
@@ -23,6 +22,21 @@ Promise.all([loadGround(), loadBirdbath(), loadWater()]).then(() => {
   renderer.addRenderStep(renderWater);
   renderer.addEventListener('click', waterHandleClick);
 });
+
+renderer.addEventListener('wheel', (ctx, e) => {
+  e.preventDefault();
+  let zenithDegrees = ctx.zenithAngle * (180 / Math.PI);
+  zenithDegrees += e.deltaY * 0.05;
+  zenithDegrees = Math.max(zenithDegrees, -93);
+  zenithDegrees = Math.min(zenithDegrees, 93);
+  ctx.zenithAngle = zenithDegrees * (Math.PI / 180);
+
+  // let azimuthDegrees = ctx.azimuthAngle * (180 / Math.PI);
+  // azimuthDegrees -= e.deltaX * 0.05;
+  // azimuthDegrees = Math.max(azimuthDegrees, -90);
+  // azimuthDegrees = Math.min(azimuthDegrees, 90);
+  // ctx.azimuthAngle = azimuthDegrees * (Math.PI / 180);
+}, { passive: false });
 
 renderer.start();
 
