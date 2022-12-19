30ba64e21d7647f59eee1a528733c20a1401bd88
Luke Taylor <luke@deentaylor.com>
2022-12-19 08:46:07 -0500

    new wave shape

--- src/compute/wave-drop.glsl
+++ src/compute/wave-drop.glsl
@@ -17,11 +17,9 @@ void main() {
   vec2 dist = (v_position - u_dropPosition) / u_dropRadius;
   float inCircle = smoothstep(1.0, 0.99, length(dist));
 
-  new_height = texture(u_height, vec2(0.5, 0.5)).r;
-
   new_height = mix(
     texture(u_height, v_position).r, // outside circle, keep original height
-    1.0 - length(dist), // inside circle, render “cone”
+    -0.5 * (1.0 - length(dist)), // inside circle, render little divet
     inCircle
   );
 }
